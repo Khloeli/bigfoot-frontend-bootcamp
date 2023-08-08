e284eed0fd3f30a0a91500fe45dd39b49375de22
khloeli <chloelihuimin@gmail.com>
2023-08-08 12:07:58 +0800

    render composer

--- src/components/SightingPage.js
+++ src/components/SightingPage.js
@@ -2,26 +2,37 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "../constants";
-// import { ListGroup } from "react-bootstrap";
-import ListGroup from "react-bootstrap/ListGroup";
+import { ListGroup, Form, Button } from "react-bootstrap";
 
 function SightingPage() {
   const [sightingIndex, setSightingIndex] = useState();
   const [sighting, setSighting] = useState();
   const [comments, setComments] = useState();
+  const [newComment, setNewComment] = useState();
 
   useEffect(() => {
     axios.get(`${BACKEND_URL}/sightings/${sightingIndex}`).then((response) => {
       setSighting(response.data);
-      console.log(response.data);
     });
     axios
       .get(`${BACKEND_URL}/sightings/${sightingIndex}/comments`)
       .then((response) => {
         setComments(response.data);
-        console.log(response.data);
       });
-  }, [sightingIndex]);
+  }, [sightingIndex, newComment]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // send date, location, notes to backend
+    axios
+      .post(`${BACKEND_URL}/sightings/${sightingIndex}/comments`, {
+        content: newComment,
+      })
+      .then(() => {
+        setNewComment("");
+      });
+  };
 
   // Get the sightingIndex param from the URL. update index
   const params = useParams();
@@ -47,7 +58,18 @@ function SightingPage() {
       <br />
       <br />
       <ListGroup>
-        <ListGroup.Item active>COMMENTS</ListGroup.Item>
+        <ListGroup.Item active>COMMENTS</ListGroup.Item>{" "}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="m-3 d-flex">
+            <Form.Control
+              type="text"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Insert new comment"
+            />
+            <Button type="submit">Submit</Button>
+          </Form.Group>
+        </Form>
         {comments &&
           comments.map((comment, index) => (
             <ListGroup.Item key={index}>{comment.content}</ListGroup.Item>
